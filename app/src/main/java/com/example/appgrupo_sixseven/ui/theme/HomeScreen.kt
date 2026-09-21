f6af62a8b28e71ec9ac4d31a5604c0aae6e60777
Pedroopy <pedrogonza626@gmail.com>
2026-09-21 10:33:07 -0300

    Mi primer commit :)

---
+++ app/src/main/java/com/example/appgrupo_sixseven/ui/theme/HomeScreen.kt
@@ -0,0 +1,135 @@
+package com.example.appgrupo_sixseven.ui.theme
+
+import android.media.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.appgrupo_sixseven.R
+import org.w3c.dom.Text
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun HomeScreen(){
+    val ColorScheme= darkColorScheme(
+        primary = Color(0xFF98222E),
+        onPrimary= Color.White,
+        onSurface = Color(0xFF333333)
+    )
+
+    MaterialTheme(
+        colorScheme = ColorScheme
+    ){
+
+    //Genera una pantalla base
+    Scaffold(
+        topBar = {
+            TopAppBar({ Text("Mi app en Kotlin",
+                color= MaterialTheme.colorScheme.onPrimary)})
+
+        }
+    ) // fin scaff
+    //Este se refiere a un espacio para que queden los componentes
+    {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(Color(0xFFF0F0F0)),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text("!Bienvenidoo",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+
+
+            )
+            Spacer(modifier = Modifier.height(66.dp))
+
+
+            androidx.compose.foundation.Image(
+                painter = painterResource(id=R.drawable.logoduoc),
+                contentDescription = "Logo App",
+                modifier= Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(66.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+
+
+            )//fin row
+            {//inicio aplicacion
+                Text("texto uno",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color= MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8F),
+                        fontWeight = FontWeight.Bold),
+                    modifier = Modifier
+                        .padding(end =8.dp)
+                )//fin texto 1
+
+                    Text("texto dos",
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            color= MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8F),
+                            fontWeight = FontWeight.Bold),
+                        modifier = Modifier
+                            .padding(end =8.dp)
+                    )//fin texto 2
+
+
+            }//fin apalicacion
+
+            Spacer(modifier = Modifier.height(66.dp))
+
+            Button(onClick = {/*Accion futura */ },
+                modifier = Modifier
+                    .fillMaxWidth(0.8F)
+
+                ){
+                Text(("Presiona"))
+            }
+
+        }//fin column
+    }
+    }
+}// fin HomeScreen
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
